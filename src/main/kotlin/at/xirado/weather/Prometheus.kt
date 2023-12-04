f0cbce80568f4a469f1f7b4fa197ebcaeb739c2b
Marcel Korzonek <bquickzz0@gmail.com>
2023-12-04 20:34:23 +0100

    Add missing metrics

--- src/main/kotlin/at/xirado/weather/Prometheus.kt
+++ src/main/kotlin/at/xirado/weather/Prometheus.kt
@@ -10,6 +10,19 @@ object Metrics {
     val humidityOutside: Gauge = Gauge.build("humidity", "Outside humidity in %").register()
     val humidityInside: Gauge = Gauge.build("humidityin", "Inside humidity in %").register()
 
+    val windSpeed: Gauge = Gauge.build("windspeed", "Wind speed in mph").register()
+    val windGust: Gauge = Gauge.build("windgust", "Wind gust in mph").register()
+    val windDir: Gauge = Gauge.build("winddir", "Wind direction").register()
+
+    val pressureRelative: Gauge = Gauge.build("relpressure", "Relative pressure").register()
+    val pressureAbsolute: Gauge = Gauge.build("abspressure", "Absolute pressure").register()
+
+    val rainRateIn: Gauge = Gauge.build("rainratein", "Rain rate").register()
+    val eventRainIn: Gauge = Gauge.build("eventrainin", "Rain event").register()
+
+    val solarRadiation: Gauge = Gauge.build("solar", "Solar radiation").register()
+    val uv: Gauge = Gauge.build("uv", "UV").register()
+
     fun init(config: Config) {
         HTTPServer(config.prometheusHost, config.prometheusPort, true)
     }
--- src/main/kotlin/at/xirado/weather/plugins/Routing.kt
+++ src/main/kotlin/at/xirado/weather/plugins/Routing.kt
@@ -8,16 +8,27 @@ import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
-    // Starting point for a Ktor app:
     routing {
         post("/weather") {
             val body = call.receiveParameters()
-            println(body["tempf"])
             body["tempf"]?.let { Metrics.temperatureOutside.set(it.toDouble()) }
             body["tempinf"]?.let { Metrics.temperatureInside.set(it.toDouble()) }
 
             body["humidity"]?.let { Metrics.humidityOutside.set(it.toDouble()) }
             body["humidityin"]?.let { Metrics.humidityInside.set(it.toDouble()) }
+
+            body["windspeedmph"]?.let { Metrics.windSpeed.set(it.toDouble()) }
+            body["windgustmph"]?.let { Metrics.windGust.set(it.toDouble()) }
+            body["winddir"]?.let { Metrics.windDir.set(it.toDouble()) }
+
+            body["baromrelin"]?.let { Metrics.pressureRelative.set(it.toDouble()) }
+            body["baromabsin"]?.let { Metrics.pressureAbsolute.set(it.toDouble()) }
+
+            body["rainratein"]?.let { Metrics.rainRateIn.set(it.toDouble()) }
+            body["eventrainin"]?.let { Metrics.eventRainIn.set(it.toDouble()) }
+
+            body["solarradiation"]?.let { Metrics.solarRadiation.set(it.toDouble()) }
+            body["uv"]?.let { Metrics.uv.set(it.toDouble()) }
 
             call.respond(HttpStatusCode.OK)
         }
